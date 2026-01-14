1f81387e176b9315f54b5567ee7b1cfadccc275a
rg <rg@tamedbeast.de>
2026-01-14 10:58:09 +0100

    Expand component scanning root

--- src/main/kotlin/sh/nunc/causa/web/CausaApplication.kt
+++ src/main/kotlin/sh/nunc/causa/web/CausaApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["sh.nunc.causa"])
 @EnableScheduling
 class CausaApplication
 
