88b5e47c57194a90d3faac80bdf6649857856cc7
rg <rg@tamedbeast.de>
2026-01-14 11:11:37 +0100

    Explicitly scan JPA repositories and entities

--- src/main/kotlin/sh/nunc/causa/web/CausaApplication.kt
+++ src/main/kotlin/sh/nunc/causa/web/CausaApplication.kt
@@ -1,10 +1,14 @@
 package sh.nunc.causa.web
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication(scanBasePackages = ["sh.nunc.causa"])
+@EnableJpaRepositories(basePackages = ["sh.nunc.causa"])
+@EntityScan(basePackages = ["sh.nunc.causa"])
 @EnableScheduling
 class CausaApplication
 
